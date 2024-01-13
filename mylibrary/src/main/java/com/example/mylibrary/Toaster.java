b434707928791e26d07d7cb56e5ccb06a0babe85
Tosty <ajaygimbhal@gmail.com>
2024-01-13 00:56:01 -0800

    soess

--- mylibrary/src/main/java/com/example/mylibrary/Toaster.java
+++ mylibrary/src/main/java/com/example/mylibrary/Toaster.java
@@ -3,6 +3,9 @@ package com.example.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.annotation.Keep;
+
+@Keep
 public class Toaster {
     public static void makeToast(Context context, String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
